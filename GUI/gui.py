a0121863018e84626203324bc79b9aef11f039d9
louisyoungx <1462648167@qq.com>
2021-11-22 18:59:25 +0800

    fix(requirements): remove pywebview requirement

--- GUI/gui.py
+++ GUI/gui.py
@@ -1,4 +1,4 @@
-import webview
+
 
 from Config.settings import config
 
@@ -8,14 +8,16 @@ PORT = config.settings("Server", "PORT")
 
 def gui():
     url = "http://{}:{}/".format(SERVER_HOST, PORT)
-    webview.create_window(PROJECT,
-                          url=url,
-                          js_api=None,
-                          width=900,
-                          height=800,
-                          resizable=True,
-                          fullscreen=False,
-                          min_size=(200, 200),
-                          background_color='#FFF',
-                          text_select=False)
-    webview.start()
+
+    # import webview
+    # webview.create_window(PROJECT,
+    #                       url=url,
+    #                       js_api=None,
+    #                       width=900,
+    #                       height=800,
+    #                       resizable=True,
+    #                       fullscreen=False,
+    #                       min_size=(200, 200),
+    #                       background_color='#FFF',
+    #                       text_select=False)
+    # webview.start()
